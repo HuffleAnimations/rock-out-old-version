c11ed68c5e58d9bbf675abfac44ff3e1780017be
HuffleAnimations <huffleanimations@gmail.com>
2019-11-07 13:24:18 +0100

    Add label to toolbar in GymListActivity

--- app/src/main/java/be/huffle/rockout/activities/GymListActivity.java
+++ app/src/main/java/be/huffle/rockout/activities/GymListActivity.java
@@ -1,18 +1,27 @@
 package be.huffle.rockout.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.util.Objects;
 
 import be.huffle.rockout.R;
 import be.huffle.rockout.adapters.GymListAdapter;
 import be.huffle.rockout.repositories.MockGymRepository;
 
-public class GymListActivity extends AppCompatActivity
+public class GymListActivity extends AppCompatActivity implements Drawer.OnDrawerItemClickListener
 {
 	private GymListAdapter adapter;
 	private MockGymRepository repository = new MockGymRepository();
@@ -35,5 +44,28 @@ public class GymListActivity extends AppCompatActivity
 				layoutManager.getOrientation());
 		gymList.addItemDecoration(itemDecoration);
 		gymList.setVisibility(View.VISIBLE);
+
+		Toolbar toolbar = findViewById(R.id.tb_main);
+		setSupportActionBar(toolbar);
+
+		new DrawerBuilder()
+				.withActivity(this)
+				.withTranslucentNavigationBar(true)
+				.withActionBarDrawerToggle(true)
+				.withDrawerGravity(Gravity.END)
+				.withSelectedItemByPosition(-1)
+				.withCloseOnClick(false)
+				.addDrawerItems(
+						new PrimaryDrawerItem().withName("Test")
+						.withOnDrawerItemClickListener(this)
+						.withSelectable(false)
+				)
+				.build();
+	}
+
+	@Override
+	public boolean onItemClick(View view, int position, IDrawerItem drawerItem)
+	{
+		return false;
 	}
 }
